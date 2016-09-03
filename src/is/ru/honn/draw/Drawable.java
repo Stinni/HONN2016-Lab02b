be0a4e4618c1541bfe31db80d92ca513bd9cbc46
Stinni <kristinnf13@ru.is>
2016-09-03 23:10:50 +0000

    Initial commit

---
+++ src/is/ru/honn/draw/Drawable.java
@@ -0,0 +1,15 @@
+package is.ru.honn.draw;
+
+/**
+ * Skilin Drawable (Drawable.java)
+ * TODO: Add description
+ *
+ * @author Kristinn Heiðar Freysteinsson
+ * @version 1, 03.09.16
+ */
+import java.awt.*;
+
+public interface Drawable
+{
+    public void draw(Graphics g);
+}
